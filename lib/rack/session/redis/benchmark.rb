3b274829ad3e4bf3a64ae8a23f0836f3255ceb20
Adrian Wolny <adrian.wolny@dawanda.com>
2014-10-02 14:21:44 +0200

    made benchmark 1.8.x compatible

--- lib/rack/session/redis/benchmark.rb
+++ lib/rack/session/redis/benchmark.rb
@@ -3,14 +3,14 @@ require 'securerandom'
 require_relative 'session_service'
 
 service = Rack::Session::Redis::SessionService.new(nil,
-                                                   redis_options: {host: 'localhost', port: 6379},
-                                                   statsd_host: '192.168.59.103:8125')
+                                                   :redis_options => {:host => 'localhost', :port => 6379},
+                                                   :statsd_host => '192.168.59.103:8125')
 
 
 def generate_session
   {
-      user_id: rand,
-      data: SecureRandom.base64
+      :user_id => rand,
+      :data => SecureRandom.base64
   }
 end
 
