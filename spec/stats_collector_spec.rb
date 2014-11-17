35bd6af2f98cfb46ff07bf25d660664ea3aa2464
Adrian Wolny <wolny101@gmail.com>
2014-11-17 11:16:57 +0100

    removed rending 'redis request duration' to statsd

--- spec/stats_collector_spec.rb
+++ spec/stats_collector_spec.rb
@@ -23,21 +23,21 @@ describe Rack::Session::Redis::StatsCollector do
   it 'should increase the timeout counter on TimeoutError' do
     expect(client).to receive(:statsd_client).and_return(statsd_client)
     expect(statsd_client).to receive(:increment).with(Rack::Session::Redis::StatsCollector::TIMEOUT_COUNTER)
+    begin
+      client.with_stats do
+        raise Redis::TimeoutError
+      end
+    rescue
+    end
+  end
 
+  it 'should re-raise Redis::TimeoutError if timeout occurred during invocation of the block' do
+    expect(client).to receive(:statsd_client).and_return(statsd_client)
+    expect(statsd_client).to receive(:increment)
     expect {
       client.with_stats do
         raise Redis::TimeoutError
       end
     }.to raise_error Redis::TimeoutError
-  end
-
-  it 'should measure the time of invocation' do
-    expect(client).to receive(:statsd_client).and_return(statsd_client)
-    duration = 1000
-    expect(statsd_client).to receive(:timing).with(Rack::Session::Redis::StatsCollector::REQUEST_DURATION, be_between(1000, 2000))
-
-    client.with_stats do
-      sleep(1)
-    end
   end
 end
